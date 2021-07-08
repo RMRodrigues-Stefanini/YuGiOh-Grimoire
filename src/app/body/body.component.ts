a85da2f4f273642b4fe8d9cf837c2c7c0f51d5aa
Rodrigo Martins Rodrigues <rmrodrigues2@stefanini.com>
2021-07-08 16:31:31 -0300

    YuGiOh - Grimoire

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.sass']
+})
+export class BodyComponent implements OnInit {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
